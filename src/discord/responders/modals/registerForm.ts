c7106309f46191e4b7d55cbe28b825a1ba00adf2
whisper062 <pgames658@gmail.com>
2025-10-18 22:20:53 -0300

    add: cargos que vão ser adicionados ao fazer registro

--- src/discord/responders/modals/registerForm.ts
+++ src/discord/responders/modals/registerForm.ts
@@ -8,9 +8,11 @@ createResponder({
   async run(interaction) {
     const { fields, member } = interaction;
     const name = fields.getTextInputValue('name');
+    const tags = ['1429166018365751498', '1428980833762676756'];
 
     try {
       await member.setNickname(name);
+      await member.roles.add(tags);
       await interaction.reply(res.success(`-# *Seu nickname foi alterado para **${name}** com sucesso!*`));
     } catch (error) {
       await interaction.reply(res.danger('-# *Ocorreu um erro ao alterar seu nickname.*'));
