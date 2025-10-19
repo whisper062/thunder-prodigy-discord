cc873af6a99e6a7a4dd72d3da65b09d4257834da
whisper062 <pgames658@gmail.com>
2025-10-18 22:42:23 -0300

    fix: alteração para o cargo correto

--- src/discord/responders/modals/registerForm.ts
+++ src/discord/responders/modals/registerForm.ts
@@ -8,7 +8,7 @@ createResponder({
   async run(interaction) {
     const { fields, member } = interaction;
     const name = fields.getTextInputValue('name');
-    const tags = ['1429166018365751498', '1428980833762676756'];
+    const tags = ['1429166018365751498', '1428982095191212065'];
 
     try {
       await member.setNickname(name);
