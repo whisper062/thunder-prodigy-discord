8ec548e3d72bbcb1c9533fa128c0a378bf15af45
whisper062 <pgames658@gmail.com>
2025-10-28 05:05:46 -0300

    feat: adicionar subcomando 'lavar' para gerenciar a lavagem de dinheiro sujo em dinheiro limpo

--- src/discord/commands/caixa/registerChannels.ts
+++ src/discord/commands/caixa/registerChannels.ts
@@ -35,15 +35,33 @@ createCommand({
                 },
             ],
         },
+        {
+            name: 'lavar',
+            description: 'Lava dinheiro sujo em dinheiro limpo usando maços ou rolos.',
+            type: ApplicationCommandOptionType.Subcommand,
+            options: [
+                {
+                    name: 'maços',
+                    description: 'Selecione a quantidade maços.',
+                    type: ApplicationCommandOptionType.Integer,
+                    required: false,
+                },
+                {
+                    name: 'rolos',
+                    description: 'Selecione a quantidade rolos.',
+                    type: ApplicationCommandOptionType.Integer,
+                    required: false,
+                },
+            ],
+        },
     ],
 
     async run(interaction) {
         const { options } = interaction;
         const doc = await db.guilds.get(interaction.guildId);
-        const canal = options.getChannel('canal', true);
 
         const row = createRow(
-               new ButtonBuilder({
+            new ButtonBuilder({
                 custom_id: 'sacar_caixa',
                 style: ButtonStyle.Secondary,
                 label: 'Sacar',
@@ -62,6 +80,7 @@ createCommand({
 
         switch (options.getSubcommand(true)) {
             case 'caixa': {
+                const canal = options.getChannel('canal', true);
                 doc.channels!.caixa = canal.id;
                 await doc.save();
                 try {
@@ -77,22 +96,56 @@ createCommand({
                 } catch (error) {
                     return interaction.reply(
                         res.danger(
-                            `-# *Não foi possível enviar uma mensagem para o canal <#${canal.id}>. Ocrreu um erro ou eu 
+                            `-# *Não foi possível enviar uma mensagem para o canal <#${canal.id}>. Ocorreu um erro ou eu 
                              não tenho permissão para enviar mensagens lá.*`,
                         ),
                     );
                 }
             }
             case 'logs': {
+                const canal = options.getChannel('canal', true);
                 doc.channels!.logsCaixa = canal.id;
                 await doc.save();
                 return interaction.reply(
                     res.success(`${constants.emojis.gear} Canal de notificações definido para: <#${canal.id}>.`),
                 );
             }
+            case 'lavar': {
+                const macos = options.getInteger('maços');
+                const rolos = options.getInteger('rolos');
+
+                if (!macos && !rolos) {
+                    return interaction.reply(res.warning('-# *Você precisa especificar maços ou rolos para lavar.*'));
+                }
+
+                if ((macos && macos < 1) || (rolos && rolos < 1)) {
+                    return interaction.reply(res.danger('-# *A quantidade deve ser maior que zero!*'));
+                }
+
+                if ((macos && doc.money!.macos < macos) || (rolos && doc.money!.rolos < rolos)) {
+                    return interaction.reply(
+                        res.danger('-# *Você não possui maços ou rolos suficientes para lavar essa quantidade.*'),
+                    );
+                }
+                if (macos) {
+                    await db.guilds.inc(interaction.guild.id, {
+                        'money.macos': -macos,
+                        'money.dinheiroSujo': -(macos * 600),
+                        'money.dinheiroLimpo': macos * 600,
+                    });
+                }
+                if (rolos) {
+                    await db.guilds.inc(interaction.guild.id, {
+                        'money.rolos': -rolos,
+                        'money.dinheiroSujo': -(rolos * 60),
+                        'money.dinheiroLimpo': rolos * 60,
+                    });
+                }
+                return interaction.reply(res.success(`-# *Lavagem concluída com sucesso!*`));
+            }
 
             default: {
-                return interaction.reply(res.warning('Subcomando inválido.'));
+                return interaction.reply(res.warning('-# *Subcomando inválido.*'));
             }
         }
     },
